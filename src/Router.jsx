8499a5abe291b729dc6defa230c1bfaa448be7ff
code with rizki <mrizki.nurrohman10@gmail.com>
2025-03-24 15:42:01 +0700

    feat: navigate app

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route, Outlet } from "react-router-dom";
-import Login from "./pages/auth/login.jsx";
-import Register from "./pages/auth/register.jsx";
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
 // import Header from "./components/Header/Header.jsx";
-import LandingPage from "./pages/movie/LandingPage.jsx";
+import LandingPage from "./pages/Movie/LandingPage.jsx";
 import "./index.css";
-import DataMovies from "./components/DataMovie/GetDataMovie.jsx";
-import Home from "./pages/movie/Home.jsx";
-import Details from "./pages/movie/Details.jsx";
+import Home from "./pages/Movie/Home.jsx";
+import Details from "./pages/Movie/Details.jsx";
+import OrderSeat from "./pages/Order/OrderSeat.jsx";
+import Payment from "./pages/Order/Payment.jsx";
+import Resultorder from "./pages/Order/Resultorder.jsx";
+import EditProfile from "./pages/HomeProfile/EditProfile.jsx";
+import NewHistory from "./pages/HomeProfile/NewHistory.jsx";
+import ResultProfile from "./pages/HomeProfile/ResultProfile.jsx";
+import MovieList from "./pages/Admin/MovieList.jsx";
+import AddMovieListAdmin from "./pages/Admin/AddMovie.jsx";
+import DashboardGraph from "./pages/Admin/Dashboard.jsx";
 
 // layout auth
 
@@ -23,12 +31,27 @@ function Router() {
     return (
         <Routes>
             <Route index element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/movies" element={<DataMovies />} />
-            {/* <Route path="/header" element={<Header />} /> */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/detail" element={<Details />} />
+            <Route path="auth">
+                <Route index element={<Login />} />
+                <Route path="register" element={<Register />} />
+            </Route>
+            <Route path="movie">
+                <Route index element={<Home />} />
+                <Route path="detail" element={<Details />} />
+                <Route path="orderseat" element={<OrderSeat />} />
+                <Route path="payment" element={<Payment />} />
+                <Route path="resultorder" element={<Resultorder />} />
+            </Route>
+            <Route path="profile">
+                <Route index element={<NewHistory />} />
+                <Route path="resultorder" element={<ResultProfile />} />
+                <Route path="editprofile" element={<EditProfile />} />
+            </Route>
+            <Route path="admin">
+                <Route index element={<MovieList />} />
+                <Route path="addmovielist" element={<AddMovieListAdmin />} />
+                <Route path="bisnis" element={<DashboardGraph />} />
+            </Route>
         </Routes>
     );
 }
